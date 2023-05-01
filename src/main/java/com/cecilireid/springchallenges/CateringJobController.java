7d2897c5083f17368e239b45f91b948f589edd1e
Matthew Chao <matthew.chao1@yahoo.com>
2023-04-30 20:10:28 -0400

    my solution to 8

--- src/main/java/com/cecilireid/springchallenges/CateringJobController.java
+++ src/main/java/com/cecilireid/springchallenges/CateringJobController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -17,12 +18,15 @@ import java.util.List;
 @RestController
 @RequestMapping("cateringJobs")
 public class CateringJobController {
-    private static final String IMAGE_API = "https://foodish-api.herokuapp.com";
+//    private static final String IMAGE_API = "https://foodish-api.herokuapp.com"; // this was not live as of 2023-04-30
+//    private static final String IMAGE_API = "https://cdn.shibe.online"; // commented out code shows getting a particular image
+    private static final String IMAGE_API = "https://shibe.online/api/shibes";
     private final CateringJobRepository cateringJobRepository;
     WebClient client;
 
     public CateringJobController(CateringJobRepository cateringJobRepository, WebClient.Builder webClientBuilder) {
         this.cateringJobRepository = cateringJobRepository;
+        this.client = webClientBuilder.baseUrl(IMAGE_API).build();
     }
 
     @GetMapping
@@ -80,7 +84,20 @@ public class CateringJobController {
         return "Job not found\n";
     }
 
+//    @GetMapping(value="/surpriseImage",
+//            produces = MediaType.IMAGE_JPEG_VALUE
+//    )
+//    public Mono<byte[]> getSurpriseImage() {
+//        return client.get()
+//                .uri("/shibes/6a6903f0fc4e4f820d04751e3f4ba260e190ead6.jpg")
+//                .retrieve()
+//                .bodyToMono(byte[].class);
+//    }
+    @GetMapping(value="/surpriseImage")
     public Mono<String> getSurpriseImage() {
-        return null;
+        return client.get()
+                .retrieve()
+                .bodyToMono(String[].class)
+                .map(strings -> strings[0]);
     }
 }
