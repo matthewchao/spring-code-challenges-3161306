0ec155cb4eb30ddca8d7a72722e7152f79d87960
Matthew Chao <matthew.chao1@yahoo.com>
2023-05-04 19:36:01 -0400

    my solution to 10

--- src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
+++ src/main/java/com/cecilireid/springchallenges/BatchConfiguration.java
@@ -6,12 +6,16 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @EnableBatchProcessing
@@ -28,24 +32,42 @@ public class BatchConfiguration {
     @Bean
     @StepScope
     public FlatFileItemReader<CateringJob> reader() {
-        FlatFileItemReader<CateringJob> reader = new FlatFileItemReader<>();
+        String[] names = new String[]{"id", "customerName", "phoneNumber", "email", "menu", "noOfGuests", "status"};
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(DelimitedLineTokenizer.DELIMITER_COMMA);
+        tokenizer.setNames(names);
+
         DefaultLineMapper<CateringJob> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setLineTokenizer(tokenizer);
+        lineMapper.setFieldSetMapper(new CateringJobMapper());
+
+        FlatFileItemReader<CateringJob> reader = new FlatFileItemReader<>();
+        reader.setLinesToSkip(1);
+        reader.setResource(new FileSystemResource("src/main/resources/upload.csv"));
+        reader.setLineMapper(lineMapper);
         return reader;
     }
 
     @Bean
     public RepositoryItemWriter<CateringJob> writer() {
-//        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
-        return null;
+        RepositoryItemWriter<CateringJob> writer = new RepositoryItemWriter<>();
+        writer.setRepository(this.repository);
+        return writer;
     }
 
     @Bean
-    public Job uploadCateringJob() {
-        return null;
+    public Job uploadCateringJob(@Autowired Step step) {
+        return jobBuilderFactory.get("uploadCateringJob")
+                .start(step)
+                .build();
     }
 
     @Bean
-    public Step step() {
-        return null;
+    public Step step(@Autowired ItemWriter<CateringJob> writer, @Autowired ItemReader<CateringJob> reader) {
+        return stepBuilderFactory.get("step")
+                .<CateringJob, CateringJob>chunk(10)
+                .reader(reader)
+                .writer(writer)
+                .build();
+
     }
 }
